de7b9713460d6d4314e274de5aa962c812e6116b
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-06-25 15:35:26 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+	
+	for (c = '0'; c<= '2'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
