47ab730a029c7fbc8d01e0e294361ae1c30e2c2d
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-06-25 23:52:22 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,12 @@
  */
 
 void print_alphabet(void)
-
 {
+    char c;
 
-	char c;
-	
-	for (c = '0'; c<= '2'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
 }
