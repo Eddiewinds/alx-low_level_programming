5d8d6ff94f57f0382571e63d851ca7ef18154d4e
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-06-26 21:53:43 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,10 +13,10 @@ int main(void)
     unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
     unsigned long half1, half2;
 
-    // Print the first two Fibonacci numbers
+    /* Print the first two Fibonacci numbers */
     printf("%lu, %lu", fib1, fib2);
 
-    // Print the first 92 Fibonacci numbers
+    /* Print the first 92 Fibonacci numbers */
     for (count = 2; count < 92; count++)
     {
         sum = fib1 + fib2;
@@ -25,13 +25,13 @@ int main(void)
         fib2 = sum;
     }
 
-    // Split the remaining numbers into two halves
+    /* Split the remaining numbers into two halves */
     fib1_half1 = fib1 / 10000000000;
     fib2_half1 = fib2 / 10000000000;
     fib1_half2 = fib1 % 10000000000;
     fib2_half2 = fib2 % 10000000000;
 
-    // Print the remaining Fibonacci numbers
+    /* Print the remaining Fibonacci numbers */
     for (count = 92; count < 98; count++)
     {
         half1 = fib1_half1 + fib2_half1;
@@ -48,7 +48,7 @@ int main(void)
         fib2_half2 = half2;
     }
 
-    // Print the final Fibonacci number without a trailing comma
+    /* Print the final Fibonacci number without a trailing comma */
     half1 = fib1_half1 + fib2_half1;
     half2 = fib1_half2 + fib2_half2;
     if (fib1_half2 + fib2_half2 > 9999999999)
