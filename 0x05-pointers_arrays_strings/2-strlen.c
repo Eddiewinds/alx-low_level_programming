a64f052ef08b223730baae69eeea1f7bb00cbbc7
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-01 12:47:00 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,15 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of 
+ * @str: The string to get the length of
  * Return: The length of @str.
  */
 size_t _strlen(const char *str)
-
 {
 	size_t length = 0;
+
 	while (*str++)
-	length++;
+		length++;
+
 	return (length);
 }
