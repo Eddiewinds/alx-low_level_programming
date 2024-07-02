4e8fca6db484989609e18b9d16c1a1bd181bd807
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-01 19:20:25 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,19 @@
  * @s: The string to print
  * Return: void
  */
-
 void print_rev(char *s)
-
 {
-	int c = 0;
-	while (s[c] != '\0')
-	{
-	c++;
-	}
-	for (c -= 1; c>= 0; c--)
-	{
-	_putchar(s[c]);
-	}
-	_putchar('\n');
+    int c = 0;
+
+    while (s[c] != '\0')
+    {
+        c++;
+    }
+
+    for (c -= 1; c >= 0; c--)
+    {
+        _putchar(s[c]);
+    }
+
+    _putchar('\n');
 }
