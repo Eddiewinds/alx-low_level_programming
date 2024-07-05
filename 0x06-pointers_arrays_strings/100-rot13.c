4af46d6f85f69898e28caf60d938763b0200a275
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-05 09:02:06 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,25 +3,28 @@
 
 /**
  * rot13 - Encodes a string using ROT13 cipher.
- * @str: The string to encode.
+ * @s: The string to encode.
  *
- * Return: A pointer to the resulting encoded string `str`.
+ * Return: A pointer to the resulting encoded string `s`.
  */
 char *rot13(char *s)
 {
     int i;
     int j;
     char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char datarot[] = "NOPQRSTUVWZTZABCDEFGHIJKLMabcdefghijklmnopqrestuvxyz";
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (s[i] == data1[j])
-			{	s[i] = datarot[j];
-				break;
-			}
-		}
-	}
+    char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; j < 52; j++)
+        {
+            if (s[i] == data1[j])
+            {
+                s[i] = datarot[j];
+                break;
+            }
+        }
+    }
+
     return (s);
 }
