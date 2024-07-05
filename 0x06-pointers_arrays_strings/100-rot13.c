edfa1bb8ddc715e4dbf010e93ba5c5d60d7962ff
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-04 18:02:22 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,17 @@
 char *rot13(char *str)
 {
     char *ptr = str;
-    char base, offset;
 
     while (*ptr != '\0')
     {
-        base = (*ptr >= 'a' && *ptr <= 'z') ? 'a' : (*ptr >= 'A' && *ptr <= 'Z') ? 'A' : 0;
-        offset = (*ptr >= 'a' && *ptr <= 'z') ? 13 : (*ptr >= 'A' && *ptr <= 'Z') ? 13 : 0;
-
-        if (base)
-            *ptr = (((*ptr - base) + offset) % 26) + base;
+        if ((*ptr >= 'a' && *ptr <= 'm') || (*ptr >= 'A' && *ptr <= 'M'))
+        {
+            *ptr = *ptr + 13;
+        }
+        else if ((*ptr >= 'n' && *ptr <= 'z') || (*ptr >= 'N' && *ptr <= 'Z'))
+        {
+            *ptr = *ptr - 13;
+        }
 
         ptr++;
     }
