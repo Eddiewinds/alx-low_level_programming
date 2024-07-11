65333a971bd9115c25aba44edbb355466c541b12
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-10 19:03:53 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,9 +8,9 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -23,11 +23,11 @@ int _strlen_recursion(char *s)
  */
 int _check_palindrome(char *s, int start, int end)
 {
-    if (start >= end)
-        return (1);
-    if (s[start] != s[end])
-        return (0);
-    return (_check_palindrome(s, start + 1, end - 1));
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (_check_palindrome(s, start + 1, end - 1));
 }
 
 /**
@@ -38,8 +38,9 @@ int _check_palindrome(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = _strlen_recursion(s);
-    if (len == 0)
-        return (1);
-    return (_check_palindrome(s, 0, len - 1));
+	int len = _strlen_recursion(s);
+
+	if (len == 0)
+		return (1);
+	return (_check_palindrome(s, 0, len - 1));
 }
