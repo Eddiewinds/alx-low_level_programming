75d5fb01ba350d2744587018423c364cfe1e76cc
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-10 18:36:43 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@
  */
 int _sqrt_recursion(int n)
 {
-    return (_sqrt(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
@@ -20,13 +20,13 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int n, int i)
 {
-    int sqrt = i * i;
+	int sqrt = i * i;
 
-    if (sqrt > n)
-        return (-1);
+	if (sqrt > n)
+		return (-1);
 
-    if (sqrt == n)
-        return (i);
+	if (sqrt == n)
+		return (i);
 
-    return (_sqrt(n, i + 1));
+	return (_sqrt(n, i + 1));
 }
