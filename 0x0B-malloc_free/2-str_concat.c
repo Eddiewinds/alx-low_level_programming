b3257c87fbe4588da48024f756ec98a3a91721bd
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-17 16:43:58 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,32 +20,32 @@ char *str_concat(char *s1, char *s2)
     if (s2 == NULL)
         s2 = "";
 
-    // Calculate the length of s1
+    /* Calculate the length of s1 */
     while (s1[i])
         i++;
 
-    // Calculate the length of s2
+    /* Calculate the length of s2 */
     while (s2[j])
         j++;
 
-    // Total length of the concatenated string
+    /* Total length of the concatenated string */
     l = i + j;
 
-    // Allocate memory for the concatenated string
+    /* Allocate memory for the concatenated string */
     s = malloc((sizeof(char) * l) + 1);
 
     if (s == NULL)
         return (NULL);
 
-    // Copy s1 into the new string
+    /* Copy s1 into the new string */
     for (k = 0; k < i; k++)
         s[k] = s1[k];
 
-    // Copy s2 into the new string
+    /* Copy s2 into the new string */
     for (k = 0; k < j; k++)
         s[i + k] = s2[k];
 
-    // Null-terminate the concatenated string
+    /* Null-terminate the concatenated string */
     s[i + j] = '\0';
 
     return (s);
