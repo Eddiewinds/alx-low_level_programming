913ac056000153e33445f8d3bef08ad7bacf5445
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-07-17 16:46:41 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,42 +11,42 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int i = 0, j = 0, k = 0, l = 0;
-    char *s;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    /* Calculate the length of s1 */
-    while (s1[i])
-        i++;
+	/* Calculate the length of s1 */
+	while (s1[i])
+		i++;
 
-    /* Calculate the length of s2 */
-    while (s2[j])
-        j++;
+	/* Calculate the length of s2 */
+	while (s2[j])
+		j++;
 
-    /* Total length of the concatenated string */
-    l = i + j;
+	/* Total length of the concatenated string */
+	l = i + j;
 
-    /* Allocate memory for the concatenated string */
-    s = malloc((sizeof(char) * l) + 1);
+	/* Allocate memory for the concatenated string */
+	s = malloc((sizeof(char) * l) + 1);
 
-    if (s == NULL)
-        return (NULL);
+	if (s == NULL)
+		return (NULL);
 
-    /* Copy s1 into the new string */
-    for (k = 0; k < i; k++)
-        s[k] = s1[k];
+	/* Copy s1 into the new string */
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
 
-    /* Copy s2 into the new string */
-    for (k = 0; k < j; k++)
-        s[i + k] = s2[k];
+	/* Copy s2 into the new string */
+	for (k = 0; k < j; k++)
+		s[i + k] = s2[k];
 
-    /* Null-terminate the concatenated string */
-    s[i + j] = '\0';
+	/* Null-terminate the concatenated string */
+	s[i + j] = '\0';
 
-    return (s);
+	return (s);
 }
